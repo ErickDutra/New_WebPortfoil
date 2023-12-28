b7727b626599837382dda9042fcde36794e831c8
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2023-12-27 22:17:47 -0300

    created method post in input label

--- newportfoil/home/models.py
+++ newportfoil/home/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 # Create your models here.
 
 class Commit_visit(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50, blank=True)
-    email = models.EmailField(max_length=254, blank=True)
+    first_name = models.CharField(max_length=50, verbose_name="Primeiro Nome:")
+    last_name = models.CharField(max_length=50, blank=True, verbose_name="Ultimo Nome:")
+    email = models.EmailField(max_length=254, blank=True, verbose_name="Email:")
     date = models.DateField(default=timezone.now)
-    text_input = models.TextField(blank=True)
+    text_input = models.TextField(blank=True,verbose_name="Mensagem:")
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
--- newportfoil/home/views.py
+++ newportfoil/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from home.data import projects
 from home.section_data import section_data
 
@@ -7,17 +7,32 @@ from home.forms import CommitForm
 
 # Create your views here.
 def index(request):
-    
     if request.method == 'POST':
-        ...
+        form = CommitForm(request.POST)
 
+        context = {
+            'form': form
+        }
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+        return render(
+            request,
+            'global\index.html',
+            context
+        )
+     
     context = {
         'form': CommitForm(),
         'section_data': section_data,
         'projects': projects,
     }
-    
+
     return render(request,
                   'global\index.html',
                   context,
                   )
+    
+    
