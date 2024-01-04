7f462f81b0335e2d18076090d4fdadc1597f8206
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2024-01-03 22:42:20 -0300

    addict message success in send message

--- newportfoil/home/views.py
+++ newportfoil/home/views.py
@@ -1,39 +1,63 @@
 from django.shortcuts import render,redirect
 from home.data import projects
 from home.section_data import section_data
-
+from django.contrib import messages
 from home.forms import CommitForm
 
 
 # Create your views here.
 def index(request):
+    
+    #method post
     if request.method == 'POST':
         form = CommitForm(request.POST)
 
+        
         context = {
-            'form': form
+            'form': form,
         }
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Obrigado pela mensagem')
             return redirect('/')
 
         return render(
             request,
             'home/index.html',
-            context
+            context,
+            
         )
 
+    #view html
     context = {
         'form': CommitForm(),
         'section_data': section_data,
         'projects': projects,
             }
-
     return render(
         request,
         'home/index.html',
         context,
         )
     
+def send(request):
+    form = CommitForm()
+    send = request.POST.get('send', 'no')
     
+    if send == 'yes':
+        return redirect( 
+            '/send'
+        )
+    #view html
+    context = {
+        'form': form,
+        'section_data': section_data,
+        'projects': projects,
+            }
+    return render(
+        request,
+        'home/index.html',
+        context,
+        )
+
